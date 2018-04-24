7938e3d2f016c2e8d0b929b6519a81f84a4b9130
sarath <sarath.sct@gmail.com>
2018-04-24 09:36:11 +0530

    Added employee model

---
+++ server/models/employee.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose');
+const validator = require('validator');
+
+const Employee = mongoose.model('Employee', {
+    code: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    doj: {
+        type: Date,
+        required: true
+    },
+
+    role: {
+        type: String,
+        required: true,
+    },
+
+    is_Active: {
+        type: Boolean,
+        default: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+
+    gender: {
+        type: String,
+        required: false
+    },
+
+    dob: {
+        type: Date,
+        required: false
+    },
+
+    mobile: {
+        type: Number,
+        required: true,
+    },
+
+    address: {
+        type: String,
+        required: false
+    },
+
+    password: {
+        type: String,
+        minlength: 6
+    },
+
+    dealer_ID: [{ type: Schema.Types.ObjectId, ref: 'Dealer' }],
+    branch_ID: [{ type: Schema.Types.ObjectId, ref: 'Branch' }]
+});
+
+module.exports = { Employee };
